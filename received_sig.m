8f1919206ef837eed931fe1692435371d4f8475a
amirmansoorian4647 <64905922+amirmansoorian4647@users.noreply.github.com>
2021-07-26 16:30:10 +0430

    Update received_sig.m

--- received_sig.m
+++ received_sig.m
@@ -1,4 +1,9 @@
 function [y, T] = received_sig(S, r_prime, r, d, fc, Diversity)
+% r_prim = location of source (it's a vector in Cartesian cordinate system)
+% r = location of receiver (it's a vector in Cartesian cordinate system)
+% d = distance between received signal & first element of array (it's a scalar)
+% fc = carrier frequency
+% Diversity = diversity of source (it's a function of angels)
     c = 3e8;
     R = r - r_prime;
     theta = atan(R(2) / R(1));
