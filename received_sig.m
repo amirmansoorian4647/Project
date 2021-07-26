e159556bbcfdb482522030d89e6004e97cd2f950
Alireza <alireza.fadakar76@gmail.com>
2021-07-26 16:12:14 +0430

    received_sig function

---
+++ received_sig.m
@@ -0,0 +1,9 @@
+function [y, T] = received_sig(S, r_prime, r, d, fc, Diversity)
+    c = 3e8;
+    R = r - r_prime;
+    theta = atan(R(2) / R(1));
+    tau = d * sin(theta) / c;
+    theta_s = theta + (R(1) <0 ) * pi;
+    norm_R = norm(R, "fro");
+    T = norm_R / c;
+    y = sqrt(Diversity(theta_s)) * exp(-1i*2*pi*fc*tau) * S / norm_R;
